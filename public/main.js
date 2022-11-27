040b61a749917038754d12118b8b7988c921b3a2
Elvin Garcia <elvingarcia19@gmail.com>
2022-11-27 16:59:49 -0500

    added SpeechSynthesis feature. When an order is complete, the customers name is called

---
+++ public/main.js
@@ -0,0 +1,26 @@
+let btn = document.querySelectorAll(".bi-check-circle")
+
+let btnArray = Array.from(btn)
+
+btnArray.forEach(element => {
+
+    element.addEventListener("click", () => {
+
+        // We'll get back something like name: elvin
+        let name = element.parentNode.parentNode.childNodes[1].innerText
+
+        // We only want elvin. so we can turn the string into an array and remove the first element name:  This leaves us with only the name
+        let nameArr = name.split(" ")
+        
+        // Remove the first element from the array which should be name:
+        nameArr.shift()
+
+        // Only element in the array should be the customers name. SO turn the array into a string
+        let nameStr = nameArr.join()
+
+        let orderReady = new SpeechSynthesisUtterance()
+        orderReady.text = `order for  ${nameStr}  is ready`
+        window.speechSynthesis.speak(orderReady)
+        window.location.reload(true)
+    })
+})
